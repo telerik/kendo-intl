48bf3ef5c49aa33d82ee154e473c701d5f55cf17
Daniel <karada@abv.bg>
2016-07-01 17:06:17 +0300

    refactor: remove nested d.ts files

--- src/numbers/format-number.d.ts
+++
@@ -1,3 +0,0 @@
-import NumberFormatOptions from './number-format-options';
-
-export default function formatNumber(value: number, format: string|NumberFormatOptions, locale?: string): string;
--- src/numbers/parse-number.d.ts
+++
@@ -1,3 +0,0 @@
-import NumberFormatOptions from './number-format-options';
-
-export default function parseNumber(value: string, locale?: string, format?: String|NumberFormatOptions): number;
