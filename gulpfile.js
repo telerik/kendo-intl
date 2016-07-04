8e0a387476d475e9eb92e468d9ecfa1cf22a124e
Daniel <karada@abv.bg>
2016-07-04 14:36:46 +0300

    chore: add task to generate default locale

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
-require('@telerik/kendo-package-tasks')(require('gulp'), 'kendo-intl');
+const gulp = require('gulp');
+const numbers = require("cldr-data/main/en/numbers.json");
+const currencies = require("cldr-data/main/en/currencies.json");
+const timeZoneNames = require("cldr-data/main/en/timeZoneNames.json");
+const calendar = require("cldr-data/main/en/ca-gregorian.json");
+const likelySubtags = require("cldr-data/supplemental/likelySubtags.json");
+const currencyData = require("cldr-data/supplemental/currencyData.json");
+const fs = require('fs');
+
+require('@telerik/kendo-package-tasks')(gulp, 'kendo-intl');
+
+gulp.task("build-default-locale", () => {
+    gulp.start("build-npm-package");
+    const cldr = require("./dist/npm/js/cldr");
+    cldr.load(likelySubtags, currencyData, numbers, currencies, timeZoneNames, calendar);
+
+    fs.writeFileSync('src/cldr/default-locale.js', "const defaultLocale = " + JSON.stringify(cldr.cldr.en, null, 4) + ";\nexport default defaultLocale;");
+});
