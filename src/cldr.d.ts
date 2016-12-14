733ce219bbe3bb091ab6b378f9dcea6ae3e80b8d
Georgi Krustev <ggkrustev@users.noreply.github.com>
2016-12-14 17:26:14 +0200

    fix: add missing type definition cldr methods

--- src/cldr.d.ts
+++ src/cldr.d.ts
@@ -5,7 +5,20 @@ export interface DateFormatNameOptions {
     standAlone?: string;
 }
 
+export interface CurrencyDisplayOptions {
+    currency: string;
+    currencyDisplay?: string;
+    value?: number;
+}
+
+export function currencyDisplay(locale: string, options: CurrencyDisplayOptions): any;
+export function currencyDisplays(locale: string, currency: string): any;
+export function currencyFractionOptions(locale: string): any;
 export function dateFormatNames(locale: string, options: DateFormatNameOptions): any;
+export function dateFormatNames(locale: string): any;
 export function firstDay(locale: string): number;
 export function load(...data: any[]): void;
+export function localeCurrency(locale: string): any;
+export function localeInfo(locale: string): any;
 export function numberSymbols(locale: string): any;
+export function territoryCurrencyCode(territory: string): any;
