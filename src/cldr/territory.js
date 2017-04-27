edd41e6405812b9ba86980f3bb512288c5a5dede
Daniel Karadachki <karada@abv.bg>
2017-04-27 07:23:22 +0300

    fix: locale territory with script or variant in the name

--- src/cldr/territory.js
+++ src/cldr/territory.js
@@ -10,22 +10,48 @@ function territoryFromName(name) {
     }
 }
 
+function territoryFromName(name, identity) {
+    const likelySubtags = cldr.supplemental.likelySubtags;
+    let parts = name.split("-");
+    if (likelySubtags) {
+        const likelyName = likelySubtags[name] || likelySubtags[parts[0]];
+        if (likelyName) {
+            parts = likelyName.split("-");
+        }
+    }
+
+    if (identity) {
+        for (let idx = parts.length - 1; idx >= 1; idx--) {
+            const part = parts[idx];
+            if (part === identity.variant || part === identity.script) {
+                parts.splice(idx, 1);
+            }
+        }
+    }
+
+    const length = parts.length;
+
+    if (length > 1) {
+        const territory = parts[length - 1];
+        return territory.toUpperCase();
+    }
+}
+
 export default function localeTerritory(info) {
     if (info.territory) {
         return info.territory;
     }
 
-    const likelySubtags = cldr.supplemental.likelySubtags;
     const name = info.name;
+    const identity = info.identity;
     let territory;
 
-    if (info.identity && info.identity.territory) {
-        territory = info.identity.territory;
-    } else if (likelySubtags && likelySubtags[name]) {
-        territory = territoryFromName(likelySubtags[name]);
+    if (identity && identity.territory) {
+        territory = identity.territory;
     } else {
-        territory = territoryFromName(name);
+        territory = territoryFromName(name, identity);
     }
+
     info.territory = territory;
 
     return territory;
