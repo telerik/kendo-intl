7b113378a48e7b18d027780df1908bdd063c9125
Daniel Karadachki <karada@abv.bg>
2017-06-09 13:08:03 +0300

    refactor: add common constants

---
+++ src/common/constants.js
@@ -0,0 +1,19 @@
+export const DECIMAL = "decimal";
+export const CURRENCY = "currency";
+export const ACCOUNTING = "accounting";
+export const PERCENT = "percent";
+export const SCIENTIFIC = "scientific";
+
+//rename to placeholder
+export const CURRENCY_SYMBOL = "$";
+export const PERCENT_SYMBOL = "%";
+export const NUMBER_SYMBOL = "n";
+
+export const LIST_SEPARATOR = ";";
+export const GROUP_SEPARATOR = ",";
+
+export const POINT = ".";
+export const EMPTY = "";
+
+export const DEFAULT_LOCALE = "en";
+
