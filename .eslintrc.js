fd04537eeaed7bae790ba3b31a70e83f92cd440e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2022-07-06 10:20:03 +0300

    chore: set up eslint for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,68 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+    root: true,
+    ignorePatterns: [
+        // Ignore build artifacts
+        '/node_modules',
+        '/dist',
+
+        // Ignore playground
+        '/examples'
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module'
     },
-    "plugins": [
-        "@typescript-eslint"
+    plugins: [
+        'import',
+        'jest'
     ],
-    "rules": {
-        "@typescript-eslint/no-explicit-any": 0
+    rules: {
+        'import/no-deprecated': [
+            'warn'
+        ],
+        'brace-style': [
+            'warn',
+            '1tbs',
+            {
+                'allowSingleLine': true
+            }
+        ],
+        'capitalized-comments': [
+            'off',
+            'never'
+        ],
+        'comma-dangle': 'error',
+        'default-case': 'error',
+        'dot-notation': 'off',
+        'id-denylist': [
+            'error',
+            'String',
+            'string',
+            'Boolean',
+            'boolean',
+            'Undefined',
+            'undefined'
+        ],
+
+        'indent': [
+            'warn',
+            4
+        ],
+        'no-empty': 'error',
+        'no-empty-function': 'off',
+        'no-redeclare': 'error',
+        'no-shadow': 'off',
+        'no-unused-expressions': 'warn',
+        'no-use-before-define': 'off',
+        'semi': 'error',
+
+        // Jest rules
+        "jest/no-disabled-tests": "off",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "warn",
+        "jest/prefer-spy-on": "warn",
+        "jest/prefer-to-be": "warn",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
     }
-}
+};
